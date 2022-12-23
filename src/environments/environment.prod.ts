9bd8973004fdb74eccc31edb870f358353583ee4
Nikola Ninov <momzzze@abv.bg>
2022-12-23 08:20:40 +0200

    some configs for the hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCXwv7TW5BGxNUMZRo_sDkeqOeisMovqMA",
+    authDomain: "blog-project-fa8fd.firebaseapp.com",
+    databaseURL: "https://blog-project-fa8fd-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "blog-project-fa8fd",
+    storageBucket: "blog-project-fa8fd.appspot.com",
+    messagingSenderId: "513924123483",
+    appId: "1:513924123483:web:00ee7f5b32c238c726287e",
+    measurementId: "G-FHH5D0HLXT"
+  }
 };
